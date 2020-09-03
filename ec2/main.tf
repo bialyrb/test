db009b1665497a1d73ba3316c61ff776a6d5bf64
rafalbialek <rafal.bialek@ascential.com>
2020-09-03 21:32:04 +0100

    s

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,13 +1,4 @@
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
+resource "local_file" "hello" {
+  content  = "Hello, Terraform"
+  filename = "hello.txt"
 }
